518c284a2ef770402e1b01d4e0bf8918c0f1680c
tommylbro <47120557+tommylbro@users.noreply.github.com>
2024-08-19 23:13:28 +0000

    first commit

---
+++ setting.py
@@ -0,0 +1,6 @@
+<import sys;sys.path.append(r'/home/codespace/.vscode-remote/extensions/fabioz.vscode-pydev-0.7.1/server/plugins/org.python.pydev.core_12.1.0.202406231238/pysrc')
+import pydevd;pydevd.patch_django_autoreload(patch_remote_debugger=True, patch_show_console=True)
+<pydevd
+<import sys;sys.path.append(r'/home/codespace/.vscode-remote/extensions/fabioz.vscode-pydev-0.7.1/server/plugins/org.python.pydev.core_12.1.0.202406231238/pysrc')
+import pydevd;pydevd.settrace()
+</pydevd>
